776383afb4cc8656e3909bf46e5c04b4044dad00
Dmitry <dementr17@yandex.ru>
2022-01-19 20:32:50 +0300

    first commit

---
+++ Public API App v2/Public API App v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Public API App v2
+//
+//  Created by Дмитрий Чепанов on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
